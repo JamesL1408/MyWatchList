650277cc241d945b9aa63609b2587196742af796
JamesL1408 <jvjllewellyn@gmail.com>
2022-08-24 15:13:13 +0200

    first commit

--- components/Header.js
+++ components/Header.js
@@ -7,6 +7,10 @@ function Header() {
 
     const menuBtn = document.getElementById('menuBtn');
     const menu = document.getElementById('menu');
+
+    menuBtn.addEventListener("click",()=>{
+      menu.classList.toggle("-translate-y-full");
+    })
   
     const [isOpen,setIsOpen] = useState(true);
     if(isOpen===true){
